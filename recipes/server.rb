22ec5d35206d5d14658a4eed7d6a6e7443448c45
paulm <pmalinow@hotmail.com>
2017-11-01 20:34:55 +0000

    patch

--- recipes/server.rb
+++ recipes/server.rb
@@ -5,7 +5,11 @@ powershell_script 'Install Web-Server' do
 end
 
 file 'c:\inetpub\wwwroot\Default.htm' do
-  content '<h1>Hello, world!</h1>'
+  content "<h1>Hello, world!</h1>
+  <h2>PLATFORM: #{node['platform']}</h2>
+  <h2>HOSTNAME: #{node['hostname']}</h2>
+  <h2>MEMORY: #{node['memory']['total']}</h2>
+  <h2>CPU Mhz: #{node['cpu']['0']['mhz']}</h2>"
 end
 
 service "w3svc" do
