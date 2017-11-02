5405146772abaf619cb090b89e84482f8932fe1f
paulm <pmalinow@hotmail.com>
2017-11-02 13:56:22 +0000

    source

--- recipes/server.rb
+++ recipes/server.rb
@@ -4,12 +4,8 @@ powershell_script 'Install Web-Server' do
   #not_if 'test-path ~/data/nodes.bak'
 end
 
-file 'c:\inetpub\wwwroot\Default.htm' do
-  content "<h1>Hello, world!</h1>
-  <h2>PLATFORM: #{node['platform']}</h2>
-  <h2>HOSTNAME: #{node['hostname']}</h2>
-  <h2>MEMORY: #{node['memory']['total']}</h2>
-  <h2>CPU Mhz: #{node['cpu']['0']['mhz']}</h2>"
+template 'c:\inetpub\wwwroot\Default.htm' do
+source 'Default.htm.erb'
 end
 
 service "w3svc" do
